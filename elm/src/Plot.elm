02fce8fa37b36d49a6b19e8cc68f637726a1bd64
Robin Hutmacher <robin.hutmacher@gmail.com>
2020-04-13 11:59:06 +0200

    Reformat

--- elm/src/Plot.elm
+++ elm/src/Plot.elm
@@ -3,17 +3,17 @@ module Plot exposing (..)
 import Axis
 import Color
 import Path exposing (Path)
+import Random
+import Route exposing (Coordinate)
 import Scale exposing (ContinuousScale)
 import Shape
+import Statistics
 import Time
 import TypedSvg exposing (g, svg, text_)
-import TypedSvg.Attributes exposing (class, fill, stroke, transform, viewBox, fontFamily, textAnchor)
-import TypedSvg.Attributes.InPx exposing (strokeWidth, fontSize)
+import TypedSvg.Attributes exposing (class, fill, fontFamily, stroke, textAnchor, transform, viewBox)
+import TypedSvg.Attributes.InPx exposing (fontSize, strokeWidth)
 import TypedSvg.Core exposing (Svg, text)
-import TypedSvg.Types exposing (Paint(..), Transform(..), AnchorAlignment(..))
-import Statistics
-import Route exposing (Coordinate)
-import Random
+import TypedSvg.Types exposing (AnchorAlignment(..), Paint(..), Transform(..))
 
 
 w : Float
@@ -31,14 +31,14 @@ padding =
     30
 
 
-defaultOrPosix : Maybe (Int, Int) -> (Time.Posix, Time.Posix)
-defaultOrPosix a = 
+defaultOrPosix : Maybe ( Int, Int ) -> ( Time.Posix, Time.Posix )
+defaultOrPosix a =
     case a of
         Nothing ->
-            (Time.millisToPosix 1577833200000, Time.millisToPosix 1586642400000)
+            ( Time.millisToPosix 1577833200000, Time.millisToPosix 1586642400000 )
 
-        Just (x , y) ->
-            (Time.millisToPosix x, Time.millisToPosix y)
+        Just ( x, y ) ->
+            ( Time.millisToPosix x, Time.millisToPosix y )
 
 
 plotDistances : List ( Time.Posix, Float ) -> Svg msg
@@ -57,52 +57,50 @@ plotDistances model =
             Axis.bottom [ Axis.tickCount 10 ] xScale
 
         yScale : ContinuousScale Float
-        yScale = 
+        yScale =
             model
                 |> List.map Tuple.second
-                |> List.maximum >> Maybe.withDefault 0
-                |> (\b -> (0, b))
-                |> Scale.linear (h - 2 * padding, 0)
+                |> List.maximum
+                >> Maybe.withDefault 0
+                |> (\b -> ( 0, b ))
+                |> Scale.linear ( h - 2 * padding, 0 )
 
         yAxis : Svg msg
         yAxis =
             Axis.left [ Axis.tickCount 5 ] yScale
 
-
         transformToLineData : ( Time.Posix, Float ) -> Maybe ( Float, Float )
         transformToLineData ( x, y ) =
             Just ( Scale.convert xScale x, Scale.convert yScale y )
-
 
         line : List ( Time.Posix, Float ) -> Path
         line data =
             List.map transformToLineData data
                 |> Shape.line Shape.linearCurve
-                
-    in 
-
+    in
     svg [ viewBox 0 0 w h ]
         [ g [ transform [ Translate (padding - 1) (h - padding) ] ]
-            [ xAxis]
+            [ xAxis ]
         , g [ transform [ Translate (padding - 1) padding ] ]
             [ yAxis ]
         , g [ transform [ Translate padding padding ], class [ "series" ] ]
             [ Path.element (line model) [ stroke <| Paint <| Color.rgb 1 0 0, strokeWidth 3, fill PaintNone ]
             ]
         , g [ transform [ Translate (w / 2) padding ] ]
-        [ text_ [ fontFamily [ "sans-serif" ], fontSize 20, textAnchor AnchorMiddle ] [ text "Route Distance" ]
-        ]
+            [ text_ [ fontFamily [ "sans-serif" ], fontSize 20, textAnchor AnchorMiddle ] [ text "Route Distance" ]
+            ]
         ]
 
 
-{-| Plots a list of routes -}
+{-| Plots a list of routes
+-}
 plotRoutes : List (List Coordinate) -> Svg msg
 plotRoutes model =
     let
         xScale : ContinuousScale Float
         xScale =
             model
-                |> List.map ((List.map .latitude) >> List.maximum >> Maybe.withDefault 0)
+                |> List.map (List.map .latitude >> List.maximum >> Maybe.withDefault 0)
                 |> Statistics.extent
                 |> Maybe.withDefault ( 48.75, 48.95 )
                 |> Scale.linear ( 0, w - 2 * padding )
@@ -112,17 +110,16 @@ plotRoutes model =
             Axis.bottom [ Axis.tickCount 5 ] xScale
 
         yScale : ContinuousScale Float
-        yScale = 
+        yScale =
             model
-                |> List.map ((List.map .longitude) >> List.maximum >> Maybe.withDefault 0)
+                |> List.map (List.map .longitude >> List.maximum >> Maybe.withDefault 0)
                 |> Statistics.extent
                 |> Maybe.withDefault ( 8.64, 9.04 )
-                |> Scale.linear (h - 2 * padding, 0)
+                |> Scale.linear ( h - 2 * padding, 0 )
 
         yAxis : Svg msg
         yAxis =
             Axis.left [ Axis.tickCount 5 ] yScale
-
 
         transformToLineData : Coordinate -> Maybe ( Float, Float )
         transformToLineData coord =
@@ -136,15 +133,14 @@ plotRoutes model =
         pathElement : List Coordinate -> Svg msg
         pathElement data =
             Path.element (line data) [ stroke <| Paint <| Color.rgb 1 0 0, strokeWidth 1, fill PaintNone ]
-
-    in 
+    in
     svg [ viewBox 0 0 w h ]
         [ g [ transform [ Translate (padding - 1) (h - padding) ] ]
-            [ xAxis]
+            [ xAxis ]
         , g [ transform [ Translate (padding - 1) padding ] ]
             [ yAxis ]
         , g [ transform [ Translate padding padding ], class [ "series" ] ] (List.map pathElement model)
         , g [ transform [ Translate (w / 2) padding ] ]
-        [ text_ [ fontFamily [ "sans-serif" ], fontSize 20, textAnchor AnchorMiddle ] [ text "Routes" ]
-        ]
+            [ text_ [ fontFamily [ "sans-serif" ], fontSize 20, textAnchor AnchorMiddle ] [ text "Routes" ]
+            ]
         ]
